9b6ab993266efd19d23aad29750afe22b0fcc14f
anirekhj <anirekhj@gmail.com>
2020-08-19 17:40:20 -0300

    nodetypename error

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-
-let idCount = links.length
-const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (parent, args) => {
-            return links.find(link => link.id === args.id)
-        }
-    },
-
-    Mutation: {
-        postLink: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            links.push(link)
-            return link
-        }
-    },
-}
+const prisma = new PrismaClient()
+// const resolvers = {
+//     Query,
+//     Mutation,
+//     User,
+//     Link
+// }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers,
+    resolvers: {
+        Query,
+    //     Mutation,
+    //     User,
+    //     Link
+    },
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
